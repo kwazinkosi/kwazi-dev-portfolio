fd4969a74b9198824256d35f74dedd54f275c832
Kwazinkosi Zwane <kwazinkosizwane@gmail.com>
2024-01-25 17:45:54 +0200

    Netlify _redirects file

--- src/pages/Contact/contact.js
+++ src/pages/Contact/contact.js
@@ -9,8 +9,8 @@ export default function contact() {
   return (
     <div className="flex flex-col min-h-screen z-10 bg-gradient-to-b from-tertiary1 to-tertiary2">
       <Navbar />
-      <div className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-5xl font-bold text-white my-20 md:my-36 lg:my-40">Contact Me</h1>
+      <div className="flex flex-col items-center justify-center text-center flex-1 px-20 text-white my-24 md:my-36 lg:my-40">
+        <h1 className="about-text text-5xl font-bold ">Contact Me</h1>
         <ContactForm/> 
       </div>
 
