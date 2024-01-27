73a1225cf860a56bce0cc9d51a8df9dd2aae2023
Kwazinkosi Zwane <kwazinkosizwane@gmail.com>
2024-01-28 00:24:44 +0200

    Favicon addition v2

--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -6,8 +6,8 @@ export default function projects() {
   return (
     <div  className="flex flex-col min-h-screen z-10 bg-gradient-to-b from-tertiary1 to-tertiary2 flex-grow w-full">
       <Navbar/>
-      <div className="flex flex-col justify-center w-full">
-        
+      <div className="flex flex-col justify-center w-full px-4 py-8 mx-auto md:px-8 md:py-16 lg:px-16 lg:py-24 xl:px-32 xl:py-32 2xl:px-48 2xl:py-40">
+        <h1 className="portfolio font-bold text-center text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl  xl:text-7xl 2xl:text-8xl">My Projects</h1>
       </div>                                                                                                                              
       <div className=" flex flex-grow flex-col justify-end w-full">
         <Footer className="" >
